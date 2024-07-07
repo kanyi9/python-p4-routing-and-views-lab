f0df1e77aee4b9dfaed47f8e966265e658a684d8
Peter <peterkanyi19193@gmail.com>
2024-07-07 17:01:13 +0300

    challenge comlepte

--- server/app.py
+++ server/app.py
@@ -4,5 +4,39 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:route>')
+def print_string(route):
+    print(route)
+    return route
+
+@app.route('/count/<int:number>')
+def count(number):
+    result = ''
+    for n in range(number):
+        result += f'{n}\n'
+    return result
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        if num2 != 0:
+            return str(num1 / num2)
+        else:
+            return 'Division by zero is not allowed.'
+    elif operation == '%':
+        return str(num1 % num2)
+    else:
+        return 'Operation not recognized. Please use one of the following: + - * div %'
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
